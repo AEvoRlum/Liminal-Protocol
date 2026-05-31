3f6d0028088cb88ba4185bae3a2b809e90a55180
AEvoRlum <2425986902@qq.com>
2026-05-31 21:16:47 +0800

    0.0.37.9
    重构了StatusEffect并构造了Fx

---
+++ src/LP/content/LPFx.java
@@ -0,0 +1,149 @@
+package LP.content;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.effect.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.units.UnitAssembler.*;
+
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+import static mindustry.graphics.Pal.*;
+
+import static LP.graphics.LPPal.*;
+
+
+public class LPFx extends Fx {
+
+    private LPFx() {
+    }
+    //Status Effects
+    public static final ParticleEffect claimEffect = new ParticleEffect() {{
+        particles = 1;
+        region = "lp-triangle";
+        lifetime = 20f;
+        length = 0f;
+        baseLength = 0f;
+        baseRotation = 0f;
+        randLength = false;
+        interp = Interp.fastSlow;
+        sizeInterp = Interp.pow3In;
+        sizeFrom = 3f;
+        sizeTo = 0f;
+        colorFrom = colorTo = Color.white;
+    }};
+
+    public static final MultiEffect disarrayEffect = new MultiEffect(
+        new WrapEffect(Fx.dynamicSpikes, Color.valueOf("E5E5E5"), 24f),
+        new ParticleEffect() {{
+            particles = 12;
+            line = true;
+            length = 30f;
+            baseLength = 0f;
+            lifetime = 50f;
+            interp = Interp.fastSlow;
+            sizeInterp = Interp.pow3In;
+            lenFrom = 8f;
+            lenTo = 0f;
+            strokeFrom = 1.5f;
+            strokeTo = 0f;
+            colorFrom = colorTo = Color.valueOf("E5E5E5");
+        }}
+    );
+
+    public static final ParticleEffect empIEffect = new ParticleEffect(){{
+        particles = 2;
+        lifetime = 30f;
+        offset = 60f;
+        cone = 360;
+        line = true;
+        useRotation = false;
+        length = -15f;
+        baseLength = 20f;
+        baseRotation = 90f;
+        interp = Interp.circleOut;
+        lenFrom = 0;
+        lenTo = 10;
+        strokeFrom = 1.5f;
+        strokeTo = 0.5f;
+        colorFrom = colorTo = Color.valueOf("9F54E4");
+    }};
+    
+    public static final ParticleEffect empIIEffect = new ParticleEffect(){{
+        particles = 2;
+        lifetime = 30f;
+        offset = 60f;
+        cone = 360;
+        line = true;
+        useRotation = false;
+        length = -20f;
+        baseLength = 25f;
+        baseRotation = 90f;
+        interp = Interp.circleOut;
+        lenFrom = 0;
+        lenTo = 10;
+        strokeFrom = 1.5f;
+        strokeTo = 0.5f;
+        colorFrom = colorTo = Color.valueOf("9F54E4");
+    }};
+
+    public static final ParticleEffect empIIIEffect = new ParticleEffect(){{
+        particles = 2;
+        lifetime = 30f;
+        offset = 60f;
+        cone = 360;
+        line = true;
+        useRotation = false;
+        length = -25f;
+        baseLength = 30f;
+        baseRotation = 90f;
+        interp = Interp.circleOut;
+        lenFrom = 0;
+        lenTo = 13;
+        strokeFrom = 1.5f;
+        strokeTo = 0.5f;
+        colorFrom = colorTo = Color.valueOf("9F54E4");
+    }};
+
+    public static final ParticleEffect flickerEffect = new ParticleEffect(){{
+        particles = 1;
+        region = "lp-square";
+        lifetime = 30f;
+        length = 0f;
+        baseLength = 0f;
+        baseRotation = 0f;
+        interp = Interp.fastSlow;
+        sizeFrom = 3f;
+        sizeTo = 0f;
+        colorFrom = colorTo = Color.valueOf("FFF589");
+    }};
+
+    public static final ParticleEffect stallEffect = new ParticleEffect(){{
+        particles = 1;
+        line = true;
+        lifetime = 30f;
+        length = -40f;
+        baseLength = 42f;
+        baseRotation = 90f;
+        offset = 180f;
+        cone = 0;
+        lenFrom = 0f;
+        lenTo = 24f;
+        strokeFrom = 0f;
+        strokeTo = 1.5f;
+        colorFrom = colorTo = Color.valueOf("E5E5E5");
+    }};
+}
---
+++ src/LP/content/LPStatusEffect.java
@@ -0,0 +1,119 @@
+package LP.content;
+
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.math.Angles;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.util.Time;
+import arc.util.Tmp;
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.content.StatusEffects;
+import mindustry.entities.Effect;
+import mindustry.entities.units.StatusEntry;
+import mindustry.game.Team;
+import mindustry.gen.Unit;
+import mindustry.graphics.Pal;
+import mindustry.type.StatusEffect;
+import LP.graphics.LPPal;
+
+import static arc.graphics.g2d.Draw.alpha;
+import static arc.graphics.g2d.Draw.color;
+import static arc.graphics.g2d.Lines.lineAngle;
+import static arc.graphics.g2d.Lines.stroke;
+import static arc.math.Angles.randLenVectors;
+
+public class LPStatusEffect{
+    
+    public static StatusEffect claim;
+    public static StatusEffect disarray;
+    public static StatusEffect empI;
+    public static StatusEffect empII;
+    public static StatusEffect empIII;
+    public static StatusEffect flicker;
+    public static StatusEffect stall;
+    public static StatusEffect stop;
+
+    private LPStatusEffect() {
+    }
+
+    public static void load(){
+        claim = new StatusEffect("Claim"){{
+            databaseTag = "amplify";
+            color = Color.white;
+            speedMultiplier = 2;
+            dragMultiplier = 0.8;
+            effectChance = 0.08;
+            effect = LPFx.claimEffect;
+        }};
+
+        disarray = new StatusEffect("Disarray"){{
+            databaseTag = "slow";
+            color = Color.valueOf("E5E5E5");
+            reloadMultiplier = 0;
+            speedMultiplier = 0;
+            buildSpeedMultiplier = 0;
+            effectChance = 0.02;
+            effect = LPFx.disarrayEffect;
+        }};
+
+        empI = new StatusEffect("Emp I"){{
+            databaseTag = "slow";
+            color = Color.valueOf("9F54E4");
+            reloadMultiplier = 0.8;
+            speedMultiplier = 0.5;
+            buildSpeedMultiplier = 0.4;
+            effectChance = 0.02;
+            effect = LPFx.empIEffect;
+        }};
+
+        empII = new StatusEffect("Emp II"){{
+            databaseTag = "slow";
+            color = Color.valueOf("9F54E4");
+            reloadMultiplier = 0.7;
+            speedMultiplier = 0.35;
+            buildSpeedMultiplier = 0.2;
+            effectChance = 0.04;
+            effect = LPFx.empIIEffect;
+        }};
+
+        empIII = new StatusEffect("Emp III"){{
+            databaseTag = "slow";
+            color = Color.valueOf("9F54E4");
+            reloadMultiplier = 0.5;
+            speedMultiplier = -0.5;
+            buildSpeedMultiplier = 0;
+            effectChance = 0.05;
+            effect = LPFx.empIIIEffect;
+        }};
+
+        flicker = new StatusEffect("Flicker"){{
+            databaseTag = "damage";
+            color = Color.valueOf("FF6464");
+            healthMultiplier = 0.85f;
+            damage = 0.2666666666666f;
+            effectChance = 0.08f;
+            effect = LPFx.flickerEffect;
+        }};
+
+        stall = new StatusEffect("Stall"){{
+            databaseTag = "slow";
+            color = Color.valueOf("737373");
+            dragMultiplier = 325000f;
+            speedMultiplier = 0f;
+            reloadMultiplier = 0f;
+            buildSpeedMultiplier = 0f;
+            effectChance = 0.8f;
+            effect = LPFx.stallEffect;
+        }};
+
+        stop = new StatusEffect("Stop"){{
+            databaseTag = "slow";
+            color = Color.white;
+            reloadMultiplier = 0f;
+        }};
+    }
+}
