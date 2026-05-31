973aae5dcb92a23b3820a2f5ee1ebeb9a48357a4
AEvoRlum <2425986902@qq.com>
2026-05-31 18:13:40 +0800

    0.0.37.9
    构造Pal

---
+++ src/LP/graphics/LPPal.java
@@ -0,0 +1,39 @@
+package LP.graphics;
+
+import arc.graphics.Color;
+import mindustry.content.Items;
+import mindustry.game.Team;
+import mindustry.graphics.Pal;
+
+public class LPPal extends Pal {
+    public static Color outline;
+    public static Color aureus;
+    public static Color aureusMid;
+    public static Color aureusDark;
+    public static Color aureusDarkMid;
+    public static Color orange;
+    public static Color orangeDark;
+    public static Color orangeRed;
+    public static Color redDark;
+    public static Color redMid;
+    public static Color redLight;
+    public static Color purple;
+
+    private LPPal() {
+    }
+    
+    static {
+        outline = Color.valueOf("262720");
+        aureus = Color.valueOf("FFF589");
+        aureusMid = Color.valueOf("FCF287");
+        aureusDark = Color.valueOf("F8F0AF");
+        aureusDarkMid = Color.valueOf("FCF8D7");
+        orange = Color.valueOf("FFB570");
+        orangeDark = Color.valueOf("EB564B");
+        orangeRed = Color.valueOf("FF5845");
+        redDark = Color.valueOf("FF4040");
+        redMid = Color.valueOf("FF6464");
+        redLight = Color.valueOf("FF9492");
+        purple = Color.valueOf("D1A4FF");
+    }
+}
