244c2656257fb1f69ac2bd460d1329983a4bdcec
AEvoRlum <2425986902@qq.com>
2026-07-20 09:28:32 +0800

    移除多余日志部分及已弃用的被注释部分
    V.test.0.0.38.4-1

--- src/ContingencyContract/ui/CCStyles.java
+++ src/ContingencyContract/ui/CCStyles.java
@@ -1,34 +1,25 @@
 package ContingencyContract.ui;
 
-import arc.util.Log;
 import arc.graphics.Color;
 import arc.scene.ui.Label.LabelStyle;
 import mindustry.ui.Styles;
 
 public class CCStyles {
-    public static LabelStyle /** FZYSGBKLabel, */
+    public static LabelStyle
     NovecentoWideLabel, BenderLabel, DefaultLabel;
 
     public static void loadStyles() {
-        /** if(CCFonts.FZYSGBK != null){
-            FZYSGBKLabel = new LabelStyle(CCFonts.FZYSGBK, Color.white);
-        }else{
-            FZYSGBKLabel = new LabelStyle(Styles.defaultLabel);
-            Log.info("CCStyles: using defaultLabel for FZYSGBKLabel");
-        } */
 
         if(CCFonts.NovecentoWide != null){
             NovecentoWideLabel = new LabelStyle(CCFonts.NovecentoWide, Color.white);
         }else{
             NovecentoWideLabel = new LabelStyle(Styles.defaultLabel);
-            Log.info("CCStyles: using defaultLabel for NovecentoWideLabel");
         }
 
         if(CCFonts.Bender != null){
             BenderLabel = new LabelStyle(CCFonts.Bender, Color.white);
         }else{
             BenderLabel = new LabelStyle(Styles.defaultLabel);
-            Log.info("CCStyles: using defaultLabel for BenderLabel");
         }
 
         DefaultLabel = new LabelStyle(CCFonts.STCN, Color.white);
